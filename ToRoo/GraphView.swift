9e14566c08382c43385a1c1acf07a586150f70c0
Adry Mirza <adrymirza@gmail.com>
2023-05-30 12:05:14 +0700

    add new file

---
+++ ToRoo/GraphView.swift
@@ -0,0 +1,20 @@
+//
+//  GraphView.swift
+//  ToRoo
+//
+//  Created by Adry Mirza on 30/05/23.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+    }
+}
